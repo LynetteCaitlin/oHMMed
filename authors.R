821a26e150eea5e12a842ab1d7442ee4437c3711
Michal Majka <michalmajka@hotmail.com>
2023-03-12 14:43:06 +0100

    Uploading temporary file "authors.R"

---
+++ authors.R
@@ -0,0 +1,18 @@
+# Authors@R
+p <- c(person(given = c("Lynette", "Caitlin"), 
+              family = "Mikula", 
+              email = "",
+              role = "aut", 
+              comment = ""),
+       person(given = "Claus", 
+              family = "Vogl", 
+              email = "",
+              role = "aut", 
+              comment = ""),
+       person(given = "Michal",
+              family = "Majka", 
+              email = "michalmajka@hotmail.com",
+              role = "aut",
+              comment = c(ORCID = "0000-0002-7524-8014")))
+       
+p
