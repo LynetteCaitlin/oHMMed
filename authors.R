6186f74268138f166460772ef0378dbb2b481c98
Michal Majka <michalmajka@hotmail.com>
2023-03-13 21:26:54 +0100

    Updating authors and description of the package

--- authors.R
+++ authors.R
@@ -3,7 +3,7 @@ p <- c(person(given = c("Lynette", "Caitlin"),
               family = "Mikula", 
               email = "",
               role = "aut", 
-              comment = c(ORCID ="0000-0002-0252-4014"))),
+              comment = c(ORCID ="0000-0002-0252-4014")),
        person(given = "Claus", 
               family = "Vogl", 
               email = "",
@@ -12,7 +12,7 @@ p <- c(person(given = c("Lynette", "Caitlin"),
        person(given = "Michal",
               family = "Majka", 
               email = "michalmajka@hotmail.com",
-              role = "aut",
+              role = c("aut", "cre"),
               comment = c(ORCID = "0000-0002-7524-8014")))
        
 p
