8ebcbaade1d529c864e18aeda5988961313cc25c
Michal Majka <michalmajka@hotmail.com>
2024-04-19 21:11:09 +0200

    Update to v1.0.3

---
+++ R/zzz.R
@@ -0,0 +1,16 @@
+.onAttach <- function(libname, pkgname) {
+  
+  v <- getNamespaceVersion("oHMMed")
+  packageStartupMessage(paste0("oHMMed ", v, " loaded"))
+  packageStartupMessage("For more information please visit: ")
+  packageStartupMessage("https://lynettecaitlin.github.io/oHMMed/")
+}
+
+
+.onDetach <- function(libpath) {
+  
+  rule <- paste0(rep("=", getOption("width")), collapse = "")
+  packageStartupMessage(rule)
+  packageStartupMessage("Thank you for using the oHMMed package!")
+  packageStartupMessage(rule)
+}
